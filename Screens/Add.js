aa12e1d164cb12017320e7c93ac79fda1b030b94
ASUS <tanzeelatariq52@gmail.com>
2023-07-26 09:31:42 +0500

    new

--- Screens/Add.js
+++ Screens/Add.js
@@ -1,238 +1,303 @@
-import { KeyboardAvoidingView } from 'react-native';
+import { KeyboardAvoidingView, Modal } from 'react-native';
 import { useState } from 'react';
-import { Text, View,TouchableOpacity,StyleSheet,TextInput } from 'react-native';
+import { Text, View,TouchableOpacity,StyleSheet,TextInput,ScrollView } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { Recurrence } from '../types/recurrence';
+import DatePicker from 'react-native-modern-datepicker';
+import { getToday,getFormatedDate } from 'react-native-modern-datepicker';
+import { FontAwesome } from "@expo/vector-icons";
+
 
 export const Add = (props) => {
-  
-  (
-    Recurrence.None
-    );
-
+ 
     return (
     
-       <>
-          <KeyboardAvoidingView
-              behavior='padding'
-              keyboardVerticalOffset={112}
-              style={{
-               flex:1,
-              
-              }}
-          >
-          <View style ={{
-       flexDirection:'row',justifyContent:'center',
-        //borderRadius: 100, 
-        alignItems: 'center',
-         width: 400 ,
-         height:50,
-         backgroundColor:'lightblue',
-         justifyContent:'center',
-         marginHorizontal:5,
-         //marginVertical:100,
-         marginTop:16,
-         //marginVertical:100,
-         flex:0,
-         borderRadius:11,
-        }}>
-          <TouchableOpacity onPress={''}
-          style={styles.navBarLeftButton}>
-       <Text style={{
-               color: 'black',
-               fontSize: 20,
-               paddingRight:260,
-               textAlign:'left'
-            }}>Amount</Text>
-      <TextInput style ={{
-         borderRadius:8,
-         backgroundColor:'teal',
-         textAlign:'right',
-         //flex:0.75,
-         paddingRight:8,
-         
-      }} placeholder='Amount'
-         placeholderTextColor='white'
-         
-      />
-        
+      <ScrollView>
+      <KeyboardAvoidingView
+          behavior='padding'
+          keyboardVerticalOffset={111}
+          style={{
+           flex:0,
           
-      
-            
-       
-    </TouchableOpacity> 
-          </View>
-          <View style ={{
-       flexDirection:'row',justifyContent:'center',
-        //borderRadius: 100, 
-        alignItems: 'center',
-         width: 400 ,
-         height:50,
-         backgroundColor:'lightblue',
-         justifyContent:'center',
-         marginHorizontal:5,
-         //marginVertical:100,
-         marginTop:16,
-         //marginVertical:100,
-         flex:0,
-         borderRadius:11,
-        }}>
-          <TouchableOpacity onPress={''}
-          style={styles.navBarLeftButton}>
-       <Text style={{
-               color: 'black',
-               fontSize: 20,
-               paddingRight:260,
-               textAlign:'left'
-            }}>Recurrence</Text>
-      <Picker  
-          //selectedValue={recurrence}
-         // onValueChange={(newRecuurence)=>setRecuurence (newRecuurence)}
-          
+          }}
       >
-        <Picker.Item label='none' value='none'/>
-        <Picker.Item label='javaScript' value='js'/>
-      </Picker>
-        
-          
-      
-            
        
-    </TouchableOpacity> 
-          </View>
-          <View style ={{
-       flexDirection:'row',justifyContent:'center',
-        //borderRadius: 100, 
-        alignItems: 'center',
-         width: 400 ,
-         height:50,
-         backgroundColor:'lightblue',
-         justifyContent:'center',
-         marginHorizontal:5,
-         //marginVertical:100,
-         marginTop:16,
-         //marginVertical:100,
-         flex:0,
-         borderRadius:11,
-        }}>
-          <TouchableOpacity onPress={''}
-          style={styles.navBarLeftButton}>
-       <Text style={{
-               color: 'black',
-               fontSize: 20,
-               paddingRight:260,
-               textAlign:'left'
-            }}>Recurrence</Text>
-      <Picker  
-          //selectedValue={recurrence}
-         // onValueChange={(newRecuurence)=>setRecuurence (newRecuurence)}
-          
-      >
-        <Picker.Item label='none' value='none'/>
-        <Picker.Item label='javaScript' value='js'/>
-      </Picker>
-        
-          
+      <View style ={{
+   flexDirection:'row',justifyContent:'center',
+    //borderRadius: 100, 
+    alignItems: 'center',
+     width: 400 ,
+     height:50,
+     backgroundColor:'lightblue',
+     justifyContent:'center',
+     marginHorizontal:5,
+     //marginVertical:100,
+     marginTop:16,
+     //marginVertical:100,
+     flex:0,
+     borderRadius:11,
+    }}>
+      <TouchableOpacity onPress={''}
+      style={styles.navBarLeftButton}>
+   <Text style={{
+           color: 'black',
+           fontSize: 20,
+           paddingRight:260,
+           textAlign:'left'
+        }}>Amount</Text>
+  <TextInput style ={{
+     borderRadius:8,
+     backgroundColor:'lightblue',
+     textAlign:'right',
+     //flex:0.75,
+     paddingRight:8,
+     
+  }} placeholder='Amount'
+     placeholderTextColor='white'
+     
+  />
+    
       
-            
-       
-    </TouchableOpacity> 
-          </View>
-          <View style ={{
-       flexDirection:'row',justifyContent:'center',
-        //borderRadius: 100, 
-        alignItems: 'center',
-         width: 400 ,
-         height:50,
-         backgroundColor:'lightblue',
-         justifyContent:'center',
-         marginHorizontal:5,
-         //marginVertical:100,
-         marginTop:16,
-         //marginVertical:100,
-         flex:0,
-         borderRadius:11,
-        }}>
-          <TouchableOpacity onPress={''}
-          style={styles.navBarLeftButton}>
-       <Text style={{
-               color: 'black',
-               fontSize: 20,
-               paddingRight:260,
-               textAlign:'left'
-            }}>Recurrence</Text>
-      <Picker  
-          //selectedValue={recurrence}
-         // onValueChange={(newRecuurence)=>setRecuurence (newRecuurence)}
-          
-      >
-        <Picker.Item label='none' value='none'/>
-        <Picker.Item label='javaScript' value='js'/>
-      </Picker>
-        
-          
-      
-            
-       
-    </TouchableOpacity> 
-          </View>
-          <View style ={{
-       flexDirection:'row',justifyContent:'center',
-        //borderRadius: 100, 
-        alignItems: 'center',
-         width: 400 ,
-         height:50,
-         backgroundColor:'lightblue',
-         justifyContent:'center',
-         marginHorizontal:5,
-         //marginVertical:100,
-         marginTop:16,
-         //marginVertical:100,
-         flex:0,
-         borderRadius:11,
-        }}>
-          <TouchableOpacity onPress={''}
-          style={styles.navBarLeftButton}>
-       <Text style={{
-               color: 'black',
-               fontSize: 20,
-               paddingRight:260,
-               textAlign:'left'
-            }}>Recurrence</Text>
-      <Picker  
-          
-          
-      >
-        <Picker.Item label='None' value='none'/>
-        <Picker.Item label='javaScript' value='js'/>
-      </Picker>
-        
-          
-      
-            
-       
-    </TouchableOpacity> 
-          </View>
-
-          </KeyboardAvoidingView>
-       </>
+  
         
    
-    );
-  } 
-  const styles= StyleSheet.create({
-   navBarLeftButton: {
-     flex: 1,
-     flexDirection: 'row',
-     alignItems: 'center',
-     justifyContent: 'center',
-     width: 100,
-     paddingLeft: 8
- 
-   },
-   buttonText: {
-     color: '',
-     fontSize: 20,
-     marginRight:10,
-   }
- })
+</TouchableOpacity> 
+      </View>
+      <View style ={{
+   flexDirection:'row',justifyContent:'center',
+    //borderRadius: 100, 
+    alignItems: 'center',
+     width: 400 ,
+     height:50,
+     backgroundColor:'lightblue',
+     justifyContent:'center',
+     marginHorizontal:5,
+     //marginVertical:100,
+     marginTop:16,
+     //marginVertical:100,
+     flex:0,
+     borderRadius:11,
+    }}>
+      <TouchableOpacity onPress={''}
+      style={styles.navBarLeftButton}>
+   <Text style={{
+           color: 'black',
+           fontSize: 20,
+           paddingRight:230,
+           textAlign:'left'
+        }}>Recurrence</Text>
+  <TextInput style ={{
+     borderRadius:8,
+     backgroundColor:'lightblue',
+     textAlign:'right',
+     //flex:0.75,
+     paddingRight:8,
+     color:'#252c6a'
+  }} placeholder='None'
+     placeholderTextColor='white'
+     
+  />
+</TouchableOpacity> 
+      </View>
+      <View style ={{
+   flexDirection:'row',justifyContent:'center',
+    //borderRadius: 100, 
+    alignItems: 'center',
+     width: 400 ,
+     height:50,
+     backgroundColor:'lightblue',
+     justifyContent:'center',
+     marginHorizontal:5,
+     //marginVertical:100,
+     marginTop:16,
+     //marginVertical:100,
+     flex:0,
+     borderRadius:11,
+    }}>
+      <TouchableOpacity onPress={''}
+      style={styles.navBarLeftButton}>
+   <Text style={{
+           color: 'black',
+           fontSize: 20,
+           paddingRight:230,
+           textAlign:'left'
+        }}>Date</Text>
+  <TextInput style ={{
+     borderRadius:8,
+     backgroundColor:'lightblue',
+     textAlign:'right',
+     //flex:0.75,
+     paddingRight:8,
+     color:'teal'
+     
+  }} placeholder='YYYY/MM/DD'
+     placeholderTextColor='white'
+     
+  />
+</TouchableOpacity> 
+      </View>
+      <View style ={{
+   flexDirection:'row',justifyContent:'center',
+    //borderRadius: 100, 
+    alignItems: 'center',
+     width: 400 ,
+     height:50,
+     backgroundColor:'lightblue',
+     justifyContent:'center',
+     marginHorizontal:5,
+     //marginVertical:100,
+     marginTop:16,
+     //marginVertical:100,
+     flex:0,
+     borderRadius:11,
+    }}>
+      <TouchableOpacity onPress={''}
+      style={styles.navBarLeftButton}>
+   <Text style={{
+           color: 'black',
+           fontSize: 20,
+           paddingRight:260,
+           textAlign:'left'
+        }}>Note</Text>
+  <TextInput style ={{
+     borderRadius:8,
+     backgroundColor:'lightblue',
+     textAlign:'right',
+     //flex:0.75,
+     paddingRight:8,
+     
+     
+  }} placeholder='Note'
+  placeholdersize={30}
+     placeholderTextColor='white'
+     
+  />
+    
+      
+  
+        
+   
+</TouchableOpacity> 
+      </View>
+      <View style ={{
+   flexDirection:'row',justifyContent:'center',
+    //borderRadius: 100, 
+    alignItems: 'center',
+     width: 400 ,
+     height:50,
+     backgroundColor:'lightblue',
+     justifyContent:'center',
+     marginHorizontal:5,
+     //marginVertical:100,
+     marginTop:16,
+     //marginVertical:100,
+     flex:0,
+     borderRadius:11,
+    }}>
+      <TouchableOpacity onPress={''}
+      style={styles.navBarLeftButton}>
+   <Text style={{
+           color: 'black',
+           fontSize: 20,
+           paddingRight:230,
+           textAlign:'left'
+        }}>Category</Text>
+  <TextInput style ={{
+     borderRadius:8,
+     backgroundColor:'lightblue',
+     textAlign:'right',
+     //flex:0.75,
+     paddingRight:8,
+     color:'#fa364a'
+     
+  }} placeholder='Category'
+     placeholderTextColor='white'
+     
+  />
+    
+      
+  
+        
+   
+</TouchableOpacity> 
+      </View>
+      <View style ={{
+   flexDirection:'row',justifyContent:'center',
+    //borderRadius: 100, 
+    alignItems: 'center',
+     width: 200 ,
+     height:40,
+     backgroundColor:'teal',
+     justifyContent:'center',
+     marginHorizontal:100,
+     //marginVertical:100,
+     margin:16,
+     //marginVertical:100,
+     flex:0,
+     borderRadius:11,
+    }}>
+      <TouchableOpacity onPress={''}
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+        
+      }}>
+   <Text style={{
+           color: 'white',
+           fontSize: 20,
+           //paddingRight:230,
+           textAlign:'center',
+           justifyContent:'center'
+        }} onPress={()=>props.navigation.navigate("Expenses")}>Submit Expenses</Text>
+  
+</TouchableOpacity> 
+      </View>
+      </KeyboardAvoidingView>
+      </ScrollView>
+   
+    
+
+);
+} 
+const styles= StyleSheet.create({
+navBarLeftButton: {
+ flex: 1,
+ flexDirection: 'row',
+ alignItems: 'center',
+ justifyContent: 'center',
+ width: 100,
+ paddingLeft: 8
+
+},
+buttonText: {
+ color: '',
+ fontSize: 20,
+ marginRight:10,
+},
+centeredView:{
+flex:1,
+marginTop:22,
+
+alignItems:'center',
+justifyContent:'center',
+},
+modalView:{
+margin:100,
+backgroundColor:'teal',
+borderRadius:20,
+width :'90%',
+padding:35,
+alignItems:'center',
+shadowColor:'#000',
+shadowOffset:{
+  width:0,
+  height:2,
+},
+shadowOpacity:0.25,
+shadowRadius:4,
+elevation:5,
+}
+})
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,52 +1,71 @@
-
-import React, {useMemo} from 'react'
-import { View } from 'react-native';
-import { Text,TouchableOpacity, } from "react-native";
-import { Swipeable } from 'react-native-gesture-handler/Swipeable';
+import React, { useMemo } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { theme } from '../theme';
-export const ListItem =  () => ({
-    label, detail, onClick, swipeToDelete, onDelete, isDestructive,
 
+const ListItem = ({
+  label,
+  detail,
+  onClick,
+  swipeToDelete,
+  onDelete,
+  isDestructive,
 }) => {
-    const item = useMemo(() => (
-        <TouchableOpacity
-            style={{
-                flexDirection: 'row',
-                justifyContent: detail ? 'space-between' : 'flex-start',
-                alignItems: 'center',
-                padding: 20,
-                borderBottomwidth: 1,
-                borderbottomColor: theme.colors.border,
-            }}
-            onPress={onClick}
+  const item = useMemo(
+    () => (
+      <TouchableOpacity
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: !!detail ? 'space-between' : 'flex-start',
+          alignItems: 'center',
+          minHeight: 44,
+          paddingHorizontal: 12,
+          borderBottomWidth: 1,
+          borderBottomColor: theme.colors.border,
+          backgroundColor: theme.colors.card,
+        }}
+        onPress={onClick}
+        disabled={!onClick}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            color: isDestructive ? theme.colors.error : 'white',
+          }}
         >
-            <Text style={{ fontSize: 18 }}>{label}</Text>
-            {detail}
-        </TouchableOpacity>
+          {label}
+        </Text>
+        {detail}
+      </TouchableOpacity>
     ),
-        []
+    [label, detail]
+  );
+
+  if (swipeToDelete) {
+    return (
+      <Swipeable
+        renderRightActions={() => (
+          <TouchableOpacity
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: 100,
+            }}
+            onPress={onDelete}
+          >
+            <Text style={{ color: 'white' }}>Delete</Text>
+          </TouchableOpacity>
+        )}
+        onSwipeableRightOpen={onDelete}
+      >
+        {item}
+      </Swipeable>
     );
-    if (swipeToDelete) {
-        return (
-            <Swipeable
-                renderRightActions={() => (
-                    <TouchableOpacity
-                        style={{
-                            backgroundColor: isDestructive ?
-                                justifyContent : 'center',
-                            alignItems: 'center',
-                            width: 100,
-                        }}
-                        onPress={onDelete}
-                    >
-                        <Text style={{ color: 'white' }}>Delete</Text>
-                    </TouchableOpacity>
-                )}
-                onSwipeableRightOpen={onDelete}
-            >
-                {item}
-            </Swipeable>
-        );
-    }
-    return item;
+  }
+
+  return item;
 };
+
+export default ListItem;
